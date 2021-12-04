e4a2e706262ad8d360a0a57f5f64f28fccede89e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-04 17:16:01 +0100

    Opérations update + delele equipeé

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ app.post('/equipes/',(req,res)=>{
     equipes.push(req.body)
     res.status(200).json(equipes);
  });
+
+
+ app.put('/equipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const equipe = equipes.find(equipe => equipe.id === id);
+    equipe.name = req.body.name;
+    equipe.country = req.body.country;
+    res.status(200).json(equipe);
+ });
+
+ app.delete('/equipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const equipe = equipes.find(equipe => equipe.id === id);
+    equipes.splice(equipes.indexOf(equipe),1)
+    res.status(200).json(equipes);
+ });
