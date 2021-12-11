cd3d9c98b9287755746327f6efda5ba0c7ffdedd
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-11 10:14:20 +0100

    version avec mongodb

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express'); // import de express
 const app = express(); // instanciation de express
-const equipes = require('./equipes.json');
+//const equipes = require('./equipes.json');
+
+/**
+ * Importation du client MongoClient & connexion à la DB
+ */
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'sesame4A';
+let db ;
+MongoClient.connect(url, function(err, client) {
+ console.log("Connexion réussi avec Mongo");
+ db = client.db(dbName);
+});
+
+/// un comment
+
 app.use(express.json());
 app.listen(83,()=>{
     console.log("BACK EXPRESS JS")
 });
-
+/*
 app.get('/equipes',(req,res)=>{
    // res.send("<h1 align=center>Hello Backend Express JS</h1>");
    res.status(200).json(equipes);
@@ -36,4 +51,4 @@ app.post('/equipes/',(req,res)=>{
     const equipe = equipes.find(equipe => equipe.id === id);
     equipes.splice(equipes.indexOf(equipe),1)
     res.status(200).json(equipes);
- });
+ });*/
