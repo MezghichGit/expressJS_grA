66201197a8bf4dd104d1509bae4dc28ecd2245ba
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-11 12:28:56 +0100

    version avec Joueur correction exo

--- index.js
+++ index.js
@@ -78,6 +78,111 @@ app.get('/equipes', (req,res) => {
                  } 
                })
                
+
+               /* TD joueur */
+
+// question1
+
+app.get('/joueurs', (req,res) => {
+   db.collection('joueur').find({}).toArray(function(err, docs
+   ){
+       if (err){
+           console.log(err)
+           throw err
+       }
+       res.status(200).json(docs)
+   })
+})
+
+app.get('/joueurs/:id', (req,res) => {
+   const id = parseInt(req.params.id)
+   db.collection('joueur').find({id}).toArray(function(err, docs
+   ){
+       if (err){
+           console.log(err)
+           throw err
+       }
+       res.status(200).json(docs)
+   })
+})
+
+app.post('/joueurs', async (req,res) => {
+ try {
+   const joueurData = req.body
+   const joueur = await db.collection ('joueur').insertOne(joueurData)
+   res.status(200).json(joueur)
+ } catch (err)
+   {
+           console.log(err)
+           throw err
+       }
+})
+
+app.put('/joueurs/:id', async (req,res) => {
+   try {
+     const id = parseInt(req.params.id)
+     const newjoueur = req.body
+     const oldjoueur = (await db.collection ('joueur').find({id}).toArray())[0]
+     oldjoueur.nom=newjoueur.nom;
+     oldjoueur.numero=newjoueur.numero;
+      await db.collection ('joueur').replaceOne({id},oldjoueur)
+
+     res.status(200).json(oldjoueur)
+   } catch (err)
+     {
+             console.log(err)
+             throw err
+         }
+})
+
+   
+app.delete('/joueurs/:id', async (req,res) => {
+ try {
+   const id = (req.params.id)
+   const joueur = await db.collection ('joueur').deleteOne({id})
+   res.status(200).json(joueur)
+ } catch (err)
+   {
+           console.log(err)
+           throw err
+       }
+})
+
+// question2
+app.get('/equipes/:id/joueurs', (req,res) => {
+   const idEquipe = parseInt(req.params.id)
+   db.collection('joueur').find({idEquipe}).toArray(function(err, docs
+   ){
+       if (err){
+           console.log(err)
+           throw err
+       }
+       res.status(200).json(docs)
+   })
+})
+// question3
+
+app.get('/joueurs/:id/equipes',async (req,res) => {
+   const id = parseInt(req.params.id)
+   const joueur =await  db.collection('joueur').findOne({id});
+
+   const equipe =await db.collection('equipe').findOne({id:joueur.idEquipe});
+   res.status(200).json(equipe)
+   
+})
+//question4
+app.get('/joueurbyName/:nom', (req,res) => {
+   const nom = req.params.nom
+   db.collection('joueur').find({nom}).toArray(function(err, docs
+   ){
+       if (err){
+           console.log(err)
+           throw err
+       }
+       res.status(200).json(docs)
+   })
+})
+
 /*
 app.get('/equipes',(req,res)=>{
    // res.send("<h1 align=center>Hello Backend Express JS</h1>");
